8bbce7f774277249a0455744e42e3c7be3f5abf8
chukwuemeka1234 <anivictor.chukwuemeka.avc@gmail.com>
2022-06-18 01:55:53 +0100

    Add the frontend for greetings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Greeting from './component/Greetings';
+import Header from './component/Header';
+import Home from './component/Home';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <Header />
+    <Routes>
+      <Route
+        path="/"
+        element={(
+          <Home />
+        )}
+      />
+      <Route
+        path="/greeting"
+        element={(
+          <Greeting />
+        )}
+      />
+    </Routes>
+  </>
+);
 
 export default App;
